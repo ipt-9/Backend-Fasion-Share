8ce97cc8765191077a68264cb6d9a2dcd3842840
Lars Emmenegger <lars_emmenegger@sluz.ch>
2024-03-14 17:22:24 +0100

    migrate dings

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('password'),
             //'remember_token' => Str::random(10),
             'description'=>fake()->text(200),
-            'picture'=>fake()->image('image.jpeg',400,300, null, false)
+            'picture'=>fake()->image('../../public/picture/img.png',400,300, null, false)
         ];
     }
 
