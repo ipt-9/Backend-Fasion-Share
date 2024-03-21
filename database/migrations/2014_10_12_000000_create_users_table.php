93ea3dfe2cb7134dd6cb2ac60cd22e19d4872a26
Lars Emmenegger <lars_emmenegger@sluz.ch>
2024-03-21 14:38:17 +0100

    test Bild

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('Email')->unique();
             $table->string('Password');
             $table->string('Description')->nullable();
-            $table->binary('Picture')->nullable();
+            $table->string('Picture')->nullable();
             $table->timestamp('updated_at')->nullable();
             $table->timestamp('created_at')->nullable();
         });
