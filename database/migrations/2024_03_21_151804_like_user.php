4b4c82c0c78560a583770dc5cca00ad1aa88c050
Lars Emmenegger <lars_emmenegger@sluz.ch>
2024-03-28 15:30:23 +0100

    test like n to n

--- database/migrations/2024_03_21_151804_like_user.php
+++ database/migrations/2024_03_21_151804_like_user.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id');
             $table->foreignId('post_id');
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('created_at')->nullable();
 
         });
     }
