dfcd41419020578984a19e0ac6992c283deba43a
Lars Emmenegger <lars_emmenegger@sluz.ch>
2024-05-02 10:19:52 +0200

    test image upload

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,9 +8,9 @@ class ImageController extends Controller
 {
     public function imageUpload(Request $request)
     {
-      //  $request->validate([
-       //     'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Example validation rules
-      //  ]);
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Example validation rules
+        ]);
 
         if ($request->hasFile('image')) {
 
