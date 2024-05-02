b8076c9920c5bbd8a83aa322624211195da86a79
Lars Emmenegger <lars_emmenegger@sluz.ch>
2024-05-02 11:05:36 +0200

    test image upload

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -9,7 +9,9 @@ class ImageController extends Controller
     public function imageUpload(Request $request)
     {
         $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif', // Example validation rules
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Example validation rules
+            //'image' => 'dimensions:max_width=1000,max_height=500,ratio=2/1',
+
         ]);
 
         if ($request->hasFile('image')) {
@@ -18,7 +20,7 @@ class ImageController extends Controller
 
             $imageName = time().'.'.$image->getClientOriginalExtension();
 
-            $image->storeAs('/public/images', $imageName); // Store the image in the storage/app/public/images directory
+            $image->storeAs('/public/images/postPictures', $imageName); // Store the image in the storage/app/public/images directory
             // You can also store it in other directories as per your requirement
             //return 'moin';
 
